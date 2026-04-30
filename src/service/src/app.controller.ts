dc403dc88f255cfec47196fc4c9df81ab0bc295a
m0az <moazkm2@gmail.com>
2026-04-30 15:11:38 +0300

    add Health Controller

--- src/service/src/app.controller.ts
+++ src/service/src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  testing(): string {
-    return 'it works!';
+  check() {
+    return { status: 'ok' };
   }
 }
