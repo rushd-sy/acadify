141ae3740c784a6dc42c10ca28319d95421daeba
Mohammed Arrata <mohammedhazim2301@gmail.com>
2026-05-09 14:51:55 +0300

    Feature: Get Student By Id added

--- src/service/src/student/student.controller.ts
+++ src/service/src/student/student.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { StudentService } from './student.service';
+import { Student } from '@prisma/client';
 
 @Controller('/api/student')
 export class StudentController {
   constructor(private studentService: StudentService) {}
+
   @Get()
-  getStudents(): string[] {
-    return ['Yehya', 'Adel', 'Mohammad', 'Moaaz'];
+  getStudents(): Promise<Student[]> {
+    return this.studentService.getStudents();
+  }
+
+  @Get(':id')
+  getStudentById(id: number): Promise<Student> {
+    return this.studentService.getStudentById(id);
   }
 }
--- src/service/src/student/student.service.ts
+++ src/service/src/student/student.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Student } from '@prisma/client';
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(private prisma: PrismaService) {}
+  async getStudents(): Promise<Student[]> {
+    return await this.prisma.student.findMany();
+  }
+
+  async getStudentById(id: number): Promise<Student> {
+    const student = await this.prisma.student.findUnique({
+      where: { id },
+    });
+    if (!student) {
+      throw new Error(`Student with id ${id} not found`);
+    }
+    return student;
+  }
+}
