a598153e85cc20ae42a562689481ef27581e3b7d
Adel Obaji <adelobaji23@gmail.com>
2026-05-12 11:40:48 -0900

    create new student

--- src/service/src/student/student.controller.ts
+++ src/service/src/student/student.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { Student } from '@prisma/client';
+import { CreateStudentDTO } from './Student.dto/create-student.dto';
 
 @Controller('/api/student')
 export class StudentController {
@@ -14,5 +15,10 @@ export class StudentController {
   @Get(':id')
   getStudentById(@Param(':id') id: number): Promise<Student> {
     return this.studentService.getStudentById(parseInt(id.toString()));
+  }
+
+  @Post()
+  createStudent(@Body() newStudent: CreateStudentDTO): Promise<Student> {
+    return this.studentService.createStudent(newStudent);
   }
 }
