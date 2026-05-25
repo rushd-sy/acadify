51f08f4c8bcbf4470a7fee28e3eba11dfc3e6146
Yehya <yehyam661@gmail.com>
2026-05-25 22:51:23 +0300

    add shadcn library

--- src/dashboard/src/App.tsx
+++ src/dashboard/src/App.tsx
@@ -1,2 +1,12 @@
-const App = () => <h1>Hello World</h1>;
-export default App;
+
+import { Button } from "@/components/ui/button"
+
+ function App()  {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
