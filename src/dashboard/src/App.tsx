0a15cd1b00880b3318481d7bf88623ac3500c9ab
Yehya <yehyam661@gmail.com>
2026-06-15 19:29:23 +0300

    add black background (just to test the formatting)

--- src/dashboard/src/App.tsx
+++ src/dashboard/src/App.tsx
@@ -2,7 +2,7 @@ import SharedLayout from './featuers/SharedLayout/components/SharedLayout';
 
 function App() {
   return (
-    <div style={{ backgroundColor: 'white' }}>
+    <div style={{ backgroundColor: 'black' }}>
       <SharedLayout />
     </div>
   );
