4621fdfc936869c743e9ce3b413e5cef0e6efe87
Yehya <yehyam661@gmail.com>
2026-06-17 16:12:08 +0300

    formating

--- src/dashboard/eslint.config.js
+++ src/dashboard/eslint.config.js
@@ -15,7 +15,7 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
-  
+
     languageOptions: {
       globals: globals.browser,
     },
