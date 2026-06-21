22fca06a964e74ce57f958dcd42f65308d54ffaa
Adel Obaji <adelobaji23@gmail.com>
2026-06-21 11:52:21 -0900

    format pnpm-lock.yaml and fix sharedlayout

--- src/dashboard/src/layout/SharedLayout.tsx
+++ src/dashboard/src/layout/SharedLayout.tsx
@@ -1,14 +1,15 @@
 import './SharedLayout.css';
 import { Outlet } from 'react-router-dom';
 import LeftNavbar from './LeftNavbar';
+import UpperNavbar from './UpperNavbar';
 export default function SharedLayout() {
   return (
     <div className="shared-layout">
       <div className="title">
         <h1>A C A D Y F I</h1>
       </div>
-      <div style={{ backgroundColor: 'black' }}>
-        Upper Nav Bar :Will be add later
+      <div className="bg-white ">
+        <UpperNavbar />
       </div>
       <div className="h-full min-h-0">
         <LeftNavbar />
