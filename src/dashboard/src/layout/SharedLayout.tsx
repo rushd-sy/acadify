daacb11b9f6ea58e7db95c225e65766c86891608
Adel Obaji <adelobaji23@gmail.com>
2026-06-21 11:34:51 -0900

    add react-route-dom

--- src/dashboard/src/layout/SharedLayout.tsx
+++ src/dashboard/src/layout/SharedLayout.tsx
@@ -1,15 +1,16 @@
 import './SharedLayout.css';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 import LeftNavbar from './LeftNavbar';
-import UpperNavbar from './UpperNavbar';
 export default function SharedLayout() {
   return (
     <div className="shared-layout">
       <div className="title">
-        <h1>A C A D Y F I</h1>
+        <Link to="">
+          <h1>A C A D Y F I</h1>
+        </Link>
       </div>
-      <div className="bg-white ">
-        <UpperNavbar />
+      <div style={{ backgroundColor: 'black' }}>
+        Upper Nav Bar :Will be add later
       </div>
       <div className="h-full min-h-0">
         <LeftNavbar />
