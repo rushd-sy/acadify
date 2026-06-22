a338d2c780b00f0487cc2dad38718f25be46835a
Yehya <yehyam661@gmail.com>
2026-06-22 23:43:36 +0300

    fix formatting

--- src/dashboard/src/pages/students-page.tsx
+++ src/dashboard/src/pages/students-page.tsx
@@ -12,14 +12,14 @@ import {
 
 const array = [
   {
-    id:'1',
+    id: '1',
     name: 'Yehya',
     secondName: 'msouty',
     number: '092323223',
     email: 'yehya@gmail.com',
   },
   {
-    id:'2',
+    id: '2',
     name: 'adel',
     secondName: 'obaji',
     number: '092323223',
@@ -32,24 +32,24 @@ export default function StudentsPage() {
     <div className="h-[500px] overflow-y-auto mt-20 bg-white w-[70%] p-20 mx-auto">
       <Table>
         <TableCaption className="text-2xl">Students Informaition</TableCaption>
-        <TableHeader >
+        <TableHeader>
           <TableRow className="text-2xl p-5">
             <TableHead>Name</TableHead>
             <TableHead>Second Name</TableHead>
             <TableHead>Number</TableHead>
             <TableHead>Email</TableHead>
-            <TableHead >Actions</TableHead>
+            <TableHead>Actions</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody className="text-xl">
           {array.map((e) => {
             return (
               <TableRow key={e.id} className="bg-gray-50 hover:bg-gray-100">
-                <TableCell  className="py-6">{e.name}</TableCell>
+                <TableCell className="py-6">{e.name}</TableCell>
                 <TableCell className="py-6">{e.secondName}</TableCell>
                 <TableCell className="py-6">{e.number}</TableCell>
-                <TableCell className="py-6" >{e.email}</TableCell>
-               <TableCell className="py-6">
+                <TableCell className="py-6">{e.email}</TableCell>
+                <TableCell className="py-6">
                   <ButtonGroup>
                     <Button className="text-lg p-4">Edit</Button>
                     <Button className="text-lg p-4">Delete</Button>
