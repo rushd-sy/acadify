f817f42598d6fc0800e3e3a1cbfb9710765041bb
m0az <moazkm2@gmail.com>
2026-06-25 14:38:46 +0300

    create student details page

--- src/dashboard/src/pages/students-details-page.tsx
+++ src/dashboard/src/pages/students-details-page.tsx
@@ -1,7 +1,59 @@
+import { useParams } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+import { ButtonGroup } from '@/components/ui/button-group';
+
+const array = [
+  {
+    id: '1',
+    name: 'Yehya',
+    secondName: 'msouty',
+    number: '092323223',
+    email: 'yehya@gmail.com',
+  },
+  {
+    id: '2',
+    name: 'adel',
+    secondName: 'obaji',
+    number: '092323223',
+    email: 'adel@gmail.com',
+  },
+];
+
 export default function StudentsDetailsPage() {
+  const { id } = useParams();
+
+  const student = array.find((e) => e.id === id);
+
+  if (!student) {
+    return <div className="mt-20 text-center text-2xl">Student Not Found</div>;
+  }
+
   return (
-    <div>
-      <h1>Student Details Page</h1>
+    <div className="bg-white w-[50%] mx-auto mt-20 p-10 rounded-lg">
+      <h1 className="text-3xl font-bold mb-8">Student Details</h1>
+
+      <div className="space-y-4 text-xl">
+        <p>
+          <strong>First Name:</strong> {student.name}
+        </p>
+
+        <p>
+          <strong>Second Name:</strong> {student.secondName}
+        </p>
+
+        <p>
+          <strong>Phone Number:</strong> {student.number}
+        </p>
+
+        <p>
+          <strong>Email:</strong> {student.email}
+        </p>
+      </div>
+
+      <ButtonGroup className="mt-8">
+        <Button>Edit</Button>
+        <Button>Delete</Button>
+      </ButtonGroup>
     </div>
   );
 }
