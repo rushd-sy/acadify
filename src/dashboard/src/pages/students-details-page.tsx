9d09c25e095169053e8faf5e4a3de09f43dcf34b
Yehya <yehyam661@gmail.com>
2026-07-01 20:14:29 +0300

    student details page

--- src/dashboard/src/pages/students-details-page.tsx
+++ src/dashboard/src/pages/students-details-page.tsx
@@ -1,7 +1,31 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { useLocation } from 'react-router-dom';
+
 export default function StudentsDetailsPage() {
+  const student = useLocation().state.student;
+  console.log(student.name);
   return (
-    <div>
-      <h1>Student Details Page</h1>
+    <div style={{display: 'flex',justifyContent: 'center'}}>
+      <Card className="mt-32  w-[90%] h-[700px] max-w-full shadow-xl bg-white  ">
+        <CardHeader>
+          <CardTitle className="flex items-center gap-2 text-3xl text-black mb-32 ml-4 mt-4">
+          
+            {student.name} {student.secondName}
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent className="space-y-4">
+          <div className="flex items-center gap-3 text-2xl mb-8 ml-16">
+         
+            <span>{student.email}</span>
+          </div>
+
+          <div className="flex items-center gap-3 text-2xl ml-16">
+           
+            <span>{student.number}</span>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- src/dashboard/src/pages/students-page.tsx
+++ src/dashboard/src/pages/students-page.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { ButtonGroup } from '@/components/ui/button-group';
+import { useNavigate } from 'react-router-dom';
 import {
   Table,
   TableBody,
@@ -9,8 +10,14 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-
-const array = [
+type Student = {
+  id: string;
+  name: string;
+  secondName: string;
+  number: string;
+  email: string;
+};
+const fakeArray: Student[] = [
   {
     id: '1',
     name: 'Yehya',
@@ -26,12 +33,14 @@ const array = [
     email: 'adel@gmail.com',
   },
 ];
-
 export default function StudentsPage() {
+  const navigate = useNavigate();
   return (
     <div className="h-[500px] overflow-y-auto mt-20 bg-white w-[70%] p-20 mx-auto">
       <Table>
-        <TableCaption className="text-2xl">Students Informaition</TableCaption>
+        <TableCaption className="text-2xl">
+          Double Click to View Student Information
+        </TableCaption>
         <TableHeader>
           <TableRow className="text-2xl p-5">
             <TableHead>Name</TableHead>
@@ -42,17 +51,39 @@ export default function StudentsPage() {
           </TableRow>
         </TableHeader>
         <TableBody className="text-xl">
-          {array.map((e) => {
+          {fakeArray.map((e) => {
             return (
-              <TableRow key={e.id} className="bg-gray-50 hover:bg-gray-100">
+              <TableRow
+                key={e.id}
+                className="bg-gray-50 hover:bg-gray-100"
+                onDoubleClick={() =>
+                  navigate(`/students/${e.id}`, {
+                    state: { student: e },
+                  })
+                }
+              >
                 <TableCell className="py-6">{e.name}</TableCell>
                 <TableCell className="py-6">{e.secondName}</TableCell>
                 <TableCell className="py-6">{e.number}</TableCell>
                 <TableCell className="py-6">{e.email}</TableCell>
                 <TableCell className="py-6">
                   <ButtonGroup>
-                    <Button className="text-lg p-4">Edit</Button>
-                    <Button className="text-lg p-4">Delete</Button>
+                    <Button
+                      className="text-lg p-4"
+                      onDoubleClick={(e) => {
+                        e.stopPropagation();
+                      }}
+                    >
+                      Edit
+                    </Button>
+                    <Button
+                      onDoubleClick={(e) => {
+                        e.stopPropagation();
+                      }}
+                      className="text-lg p-4"
+                    >
+                      Delete
+                    </Button>
                   </ButtonGroup>
                 </TableCell>
               </TableRow>
