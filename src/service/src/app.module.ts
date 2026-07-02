319de1c4fd16e2cfa85c7a9ce1c3b32d30eec3c0
Adel Obaji <adelobaji23@gmail.com>
2026-07-02 14:03:57 -0900

    setup auth module without logic

--- src/service/src/app.module.ts
+++ src/service/src/app.module.ts
@@ -4,8 +4,9 @@ import { PrismaService } from './prisma/prisma.service';
 import { StudentService } from './student/student.service';
 import { StudentController } from './student/student.controller';
 import { StudentMapper } from './student/student.mapper';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [HealthCheckController, StudentController],
   providers: [StudentService, StudentMapper, PrismaService],
 })
