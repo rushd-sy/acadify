95b16b883cc2f36b2e2d4fcc80deef3d1ca62b77
Mohammed Arrata <mohammedhazim2301@gmail.com>
2026-07-06 06:55:25 +0300

    Refactor: Implement Student Domain

--- src/service/src/student/student.service.ts
+++ src/service/src/student/student.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { StudentMapper } from './student.mapper';
 import type { CreateStudentDto, StudentDetailsDto, StudentDto } from 'dtos';
+import { createStudentDomain } from '../domain/student.domain';
 @Injectable()
 export class StudentService {
   constructor(
@@ -24,12 +25,15 @@ export class StudentService {
   }
 
   async createStudent(data: CreateStudentDto): Promise<StudentDto> {
-    const persistenceData = this.mapper.toPersistence(data);
+    const newStudentDomain = this.mapper.toNewStudentDomain(data);
+    const validStudent = createStudentDomain(newStudentDomain);
+    const persistenceData = this.mapper.toPresistence(validStudent);
     const student = await this.prisma.student.create({
       data: persistenceData,
     });
     return this.mapper.toStudentDTO(student);
   }
+
   async deleteStudent(id: number): Promise<void> {
     await this.prisma.student.delete({
       where: { id },
