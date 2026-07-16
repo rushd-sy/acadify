aeacc3835b29ce8dd0a58daf612a8515bda0e5fb
m0az <moazkm2@gmail.com>
2026-07-16 03:30:51 +0300

    add primitive auth service

--- src/service/src/auth/services/auth.service.ts
+++ src/service/src/auth/services/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from 'dtos';
+import { UserRepository } from '../data/user.repository';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async login(loginDto: LoginDto): Promise<User> {
+    const user = await this.userRepository.getUser(loginDto.email);
+
+    if (!user) {
+      throw new Error('Invalid email or password');
+    }
+
+    return user;
+  }
+}
