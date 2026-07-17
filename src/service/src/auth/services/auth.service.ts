418c693070f5e42f8e1496c31ce1454139c81f5c
Ahmad Moaz <moazkm2@gmail.com>
2026-07-17 21:13:11 +0300

    add primitive auth service (#56)
    * add primitive auth service
    
    * add base & user not found error handling

--- src/service/src/auth/services/auth.service.ts
+++ src/service/src/auth/services/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from 'dtos';
+import { UserRepository } from '../data/user.repository';
+import { User } from '@prisma/client';
+import { UserNotFoundError } from '../domain/errors';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async login(loginDto: LoginDto): Promise<User> {
+    const user = await this.userRepository.getUser(loginDto.email);
+
+    if (!user) {
+      throw new UserNotFoundError();
+    }
+
+    return user;
+  }
+}
