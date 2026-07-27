6affc685bd0df01a718af4cd6aef7d4485c85df0
Adel Obaji <adelobaji23@gmail.com>
2026-07-27 23:33:53 +0300

    add jwt guard to protected routes

--- src/service/src/auth/api/auth.controller.ts
+++ src/service/src/auth/api/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { LoginDto, LoginResultDto } from 'dtos';
 import { AuthService } from '../services/auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,13 @@ export class AuthController {
   @Post('login')
   async login(@Body() loginDto: LoginDto): Promise<LoginResultDto> {
     return this.authService.login(loginDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('mock')
+  mock() {
+    return {
+      message: 'Hello',
+    };
   }
 }
