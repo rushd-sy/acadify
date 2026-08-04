e97bcf674870521ada61fc66f30323d6169243dc
Mohammed Arrata <mohammedhazim2301@gmail.com>
2026-08-04 11:33:09 +0300

    Apply formatting

--- src/dashboard/src/services/auth.service.ts
+++ src/dashboard/src/services/auth.service.ts
@@ -3,7 +3,7 @@ import type { LoginDto } from 'dtos';
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const authService = {
-  login: async (credentials: LoginDto): Promise<{ message: string}> => {
+  login: async (credentials: LoginDto): Promise<{ message: string }> => {
     const response = await fetch(`${API_URL}/auth/login`, {
       method: 'POST',
       headers: {
