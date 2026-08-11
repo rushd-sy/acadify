a77977d6472a178421e216a4bec6fb23017dd7de
Adel Obaji <adelobaji23@gmail.com>
2026-08-11 15:49:48 +0300

    add Get endpoint for current user

--- src/service/src/auth/api/auth.controller.ts
+++ src/service/src/auth/api/auth.controller.ts
@@ -1,10 +1,26 @@
-import { Controller, Get, Post, Body, UseGuards, Res } from '@nestjs/common';
-import { LoginDto } from 'dtos';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  Res,
+  Req,
+} from '@nestjs/common';
+import { LoginDto, UserDto } from 'dtos';
 import { AuthService } from '../services/auth.service';
 import { AuthGuard } from '@nestjs/passport';
-import type { Response } from 'express';
+import type { Response, Request } from 'express';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
 const ONE_DAY_MS = 1000 * 60 * 60 * 24;
+
+type AuthenticatedRequest = Request & {
+  user: {
+    id: number;
+    email: string;
+  };
+};
 
 @Controller('auth')
 export class AuthController {
@@ -30,5 +46,11 @@ export class AuthController {
   @Get('test')
   test() {
     return { status: 'ok' };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getCurrentUser(@Req() req: AuthenticatedRequest): Promise<UserDto> {
+    return this.authService.getCurrentUser(req.user);
   }
 }
