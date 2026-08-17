84dbba6a7eed29c404a6a3febd9f05b587656427
Mohammed Arrata <mohammedhazim2301@gmail.com>
2026-08-18 01:43:36 +0300

    Edit the use migration

--- src/service/prisma/migrations/20260817224032_refactor_user_student/migration.sql
+++ src/service/prisma/migrations/20260817224032_refactor_user_student/migration.sql
@@ -7,9 +7,9 @@
   - You are about to drop the column `lastName` on the `Student` table. All the data in the column will be lost.
   - You are about to drop the column `phoneNumber` on the `Student` table. All the data in the column will be lost.
   - You are about to drop the column `fullName` on the `User` table. All the data in the column will be lost.
-  - A unique constraint covering the columns `[UserId]` on the table `Student` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[userId]` on the table `Student` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[phoneNumber]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `UserId` to the `Student` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `userId` to the `Student` table without a default value. This is not possible if the table is not empty.
   - Added the required column `firstName` to the `User` table without a default value. This is not possible if the table is not empty.
   - Added the required column `lastName` to the `User` table without a default value. This is not possible if the table is not empty.
   - Added the required column `phoneNumber` to the `User` table without a default value. This is not possible if the table is not empty.
@@ -28,7 +28,7 @@ DROP COLUMN "firstName",
 DROP COLUMN "hashedPassword",
 DROP COLUMN "lastName",
 DROP COLUMN "phoneNumber",
-ADD COLUMN     "UserId" INTEGER NOT NULL;
+ADD COLUMN     "userId" INTEGER NOT NULL;
 
 -- AlterTable
 ALTER TABLE "User" DROP COLUMN "fullName",
@@ -39,10 +39,10 @@ ADD COLUMN     "phoneNumber" TEXT NOT NULL,
 ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Student_UserId_key" ON "Student"("UserId");
+CREATE UNIQUE INDEX "Student_userId_key" ON "Student"("userId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_phoneNumber_key" ON "User"("phoneNumber");
 
 -- AddForeignKey
-ALTER TABLE "Student" ADD CONSTRAINT "Student_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Student" ADD CONSTRAINT "Student_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
