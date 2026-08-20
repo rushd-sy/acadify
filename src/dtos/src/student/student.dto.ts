395bea0eb676e25d7370b02e886cfd5423c68363
Mohammed Arrata <mohammedhazim2301@gmail.com>
2026-08-21 02:54:40 +0300

    Feature: Add roles for dtos

--- src/dtos/src/student/student.dto.ts
+++ src/dtos/src/student/student.dto.ts
@@ -1,7 +1,10 @@
+import { Role } from '../auth/role.enum';
+
 export class StudentDto {
   id!: number;
   userId!: number;
   firstName!: string;
   lastName!: string;
   email!: string;
+  role?: Role;
 }
--- src/dtos/src/user/user.dto.ts
+++ src/dtos/src/user/user.dto.ts
@@ -1,7 +1,10 @@
+import { Role } from '../auth/role.enum';
+
 export class UserDto {
   id!: number;
   firstName!: string;
   lastName!: string;
   email!: string;
   phoneNumber!: string;
+  role?: Role;
 }
