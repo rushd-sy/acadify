3003f01b21f3065af813cd2af0094a43ad6b32dc
Mohammed Arrata <mohammedhazim2301@gmail.com>
2026-09-01 15:46:20 +0300

    Feature: Add Loader to Yes button

--- src/dashboard/src/components/ui/delete-student-modal.tsx
+++ src/dashboard/src/components/ui/delete-student-modal.tsx
@@ -1,3 +1,5 @@
+import { Spinner } from '@/components/ui/spinner';
+
 type DeleteStudentModalProps = {
   open: boolean;
   studentName: string;
@@ -40,30 +42,7 @@ function DeleteStudentModal({
             className="rounded-lg bg-red-500 px-4 py-2 text-white hover:bg-red-600"
             disabled={isLoading}
           >
-            {isLoading ? (
-              <svg
-                className="h-5 w-5 animate-spin text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-            ) : (
-              'Yes'
-            )}
+            {isLoading ? <Spinner className="text-white" /> : 'Yes'}
           </button>
         </div>
       </div>
