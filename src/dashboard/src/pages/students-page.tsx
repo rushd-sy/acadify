5b9f5c84ae0aa6a700443d3d75b9f118276dca9e
Mohammed Arrata <mohammedhazim2301@gmail.com>
2026-09-03 20:30:51 +0300

    Chore: Update students list in students page

--- src/dashboard/src/pages/students-page.tsx
+++ src/dashboard/src/pages/students-page.tsx
@@ -65,83 +65,83 @@ export default function StudentsPage() {
     setDeleteError(null);
   };
 
- return (
-   <div className="container mx-auto mt-20 max-w-5xl px-4">
-     <div className="rounded-md border bg-white shadow-sm overflow-x-auto">
-       <Table>
-         <TableCaption className="pb-4">
-           A list of registered students.
-         </TableCaption>
-         <TableHeader>
-           <TableRow>
-             <TableHead>Name</TableHead>
-             <TableHead>Number</TableHead>
-             <TableHead>Email</TableHead>
-             <TableHead className="text-right">Actions</TableHead>
-           </TableRow>
-         </TableHeader>
+  return (
+    <div className="container mx-auto mt-20 max-w-5xl px-4">
+      <div className="rounded-md border bg-white shadow-sm overflow-x-auto">
+        <Table>
+          <TableCaption className="pb-4">
+            A list of registered students.
+          </TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Number</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
+            </TableRow>
+          </TableHeader>
 
-         <TableBody>
-           {students.map((e) => {
-             return (
-               <TableRow 
-               key={e.id}
-               className="cursor-pointer"
-               onClick={() => navigate(`/students/${e.id}`)}
-               >
-                 <TableCell className="font-medium">
-                     {e.name} {e.secondName}
-                 </TableCell>
-                 <TableCell>{e.number}</TableCell>
-                 <TableCell>{e.email}</TableCell>
+          <TableBody>
+            {students.map((e) => {
+              return (
+                <TableRow
+                  key={e.id}
+                  className="cursor-pointer"
+                  onClick={() => navigate(`/students/${e.id}`)}
+                >
+                  <TableCell className="font-medium">
+                    {e.name} {e.secondName}
+                  </TableCell>
+                  <TableCell>{e.number}</TableCell>
+                  <TableCell>{e.email}</TableCell>
 
-                 <TableCell className="text-right">
-                   <div className="flex justify-end gap-2">
-                     <Button
-                       variant="secondary"
-                       className="h-8 px-3 text-sm"
-                       onClick={(event) => { 
-                        event.stopPropagation();
-                        setStudentToUpdate(e.id);
-                       }}
-                     >
-                       Edit
-                     </Button>
-                     <Button
-                       variant="secondary"
-                       className="h-8 px-3 text-sm"
-                       onClick={(event) => {
-                        event.stopPropagation();
-                        setStudentToDelete(e.id);
-                       }}
-                     >
-                       Delete
-                     </Button>
-                   </div>
-                 </TableCell>
-               </TableRow>
-             );
-           })}
-         </TableBody>
-       </Table>
-     </div>
+                  <TableCell className="text-right">
+                    <div className="flex justify-end gap-2">
+                      <Button
+                        variant="secondary"
+                        className="h-8 px-3 text-sm"
+                        onClick={(event) => {
+                          event.stopPropagation();
+                          setStudentToUpdate(e.id);
+                        }}
+                      >
+                        Edit
+                      </Button>
+                      <Button
+                        variant="secondary"
+                        className="h-8 px-3 text-sm"
+                        onClick={(event) => {
+                          event.stopPropagation();
+                          setStudentToDelete(e.id);
+                        }}
+                      >
+                        Delete
+                      </Button>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
 
-     <DeleteStudentModal
-       open={!!studentToDelete}
-       studentName={
-         students.find((st) => st.id === studentToDelete)?.name || ''
-       }
-       onYes={handleConfirmDelete}
-       onNo={handleCloseModal}
-       isLoading={isDeleting}
-       error={deleteError}
-     />
+      <DeleteStudentModal
+        open={!!studentToDelete}
+        studentName={
+          students.find((st) => st.id === studentToDelete)?.name || ''
+        }
+        onYes={handleConfirmDelete}
+        onNo={handleCloseModal}
+        isLoading={isDeleting}
+        error={deleteError}
+      />
 
-     <UpdateStudentModal
-       open={!!studentToUpdate}
-       studentId={studentToUpdate || ''}
-       onClose={() => setStudentToUpdate(null)}
-     />
-   </div>
- );
+      <UpdateStudentModal
+        open={!!studentToUpdate}
+        studentId={studentToUpdate || ''}
+        onClose={() => setStudentToUpdate(null)}
+      />
+    </div>
+  );
 }
