b39d32e59fdc1703713991640da08bcc109ded91
Mohammed Arrata <mohammedhazim2301@gmail.com>
2026-09-09 13:46:10 +0300

    Feature: Add domain, repository, mapper and module

--- src/service/src/grade/grade.module.ts
+++ src/service/src/grade/grade.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GradeController } from './api/grade.controller';
 import { GradeService } from './services/grade.service';
+import { GradeRepository } from './data/grade.repository';
+import { GradeMapper } from './mappers/grade.mapper';
 
 @Module({
   controllers: [GradeController],
-  providers: [GradeService],
+  providers: [GradeService, GradeRepository, GradeMapper],
 })
 export class GradeModule {}
