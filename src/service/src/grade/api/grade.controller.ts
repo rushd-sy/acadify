3e8cec45ac5fcf60c944bacaa444d9e68721747a
Mohammed Arrata <mohammedhazim2301@gmail.com>
2026-09-10 22:55:15 +0300

    Feature: implement grade service login and endpoints

--- src/service/src/grade/api/grade.controller.ts
+++ src/service/src/grade/api/grade.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { GradeService } from '../services/grade.service';
+import { CreateGradeDto, GradeDto } from 'dtos';
 
 @Controller('grades')
 export class GradeController {
-  constructor(private readonly gradeService: GradeService) {}
+  constructor(private readonly service: GradeService) {}
+
+  @Post()
+  create(@Body() createGradeDto: CreateGradeDto): Promise<GradeDto> {
+    return this.service.create(createGradeDto);
+  }
+
+  @Get()
+  findAll(): Promise<GradeDto[]> {
+    return this.service.findAll();
+  }
+
+  @Get('name/:name')
+  findByName(@Param('name') name: string): Promise<GradeDto> {
+    return this.service.findByName(name);
+  }
 }
--- src/service/src/grade/services/grade.service.ts
+++ src/service/src/grade/services/grade.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
+import { GradeRepository } from '../data/grade.repository';
+import { GradeMapper } from '../mappers/grade.mapper';
+import { CreateGradeDto, GradeDto } from 'dtos';
 
 @Injectable()
-export class GradeService {}
+export class GradeService {
+  constructor(
+    private readonly repository: GradeRepository,
+    private readonly mapper: GradeMapper,
+  ) {}
+
+  async create(createGradeDto: CreateGradeDto): Promise<GradeDto> {
+    const existingGrade = await this.repository.findByName(createGradeDto.name);
+
+    if (existingGrade) {
+      throw new ConflictException(
+        `Grade with name ${createGradeDto.name} already esists.`,
+      );
+    }
+    const gradeDomain = await this.repository.create(createGradeDto);
+    return this.mapper.toDto(gradeDomain);
+  }
+
+  async findByName(name: string): Promise<GradeDto> {
+    const gradeDomain = await this.repository.findByName(name);
+
+    if (!gradeDomain) {
+      throw new NotFoundException(`Grade with name ${name} does not exist.`);
+    }
+
+    return this.mapper.toDto(gradeDomain);
+  }
+
+  async findAll(): Promise<GradeDto[]> {
+    const gradesDomain = await this.repository.findAll();
+    return this.mapper.toDtoList(gradesDomain);
+  }
+}
