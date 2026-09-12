e14ffafbfb8073d9a3b960120dd11014278043d9
Mohammed Arrata <mohammedhazim2301@gmail.com>
2026-09-12 05:14:53 +0300

    Feature: Add Curriculum domain, mapper, repository and module

--- src/service/src/curriculum/curriculum.module.ts
+++ src/service/src/curriculum/curriculum.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CurriculumController } from './api/curriculum.controller';
-import { CurriculumService } from './services/curriculum.service';
 import { CurriculumRepository } from './data/curriculum.repository';
 import { CurriculumMapper } from './mappers/curriculum.mapper';
 
 @Module({
-  controllers: [CurriculumController],
-  providers: [CurriculumService, CurriculumRepository, CurriculumMapper],
+  controllers: [],
+  providers: [CurriculumRepository, CurriculumMapper],
 })
 export class CurriculumModule {}
