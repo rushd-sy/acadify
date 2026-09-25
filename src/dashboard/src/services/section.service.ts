cb6a8675ed1a938cac533ed99eaa628be59db811
Ahmad Moaz <moazkm2@gmail.com>
2026-09-25 15:53:08 +0300

    Feat/view sections frontend (#139)
    * implement view sections backend
    
    * implement sections frontend page
    
    * simple fix

--- src/dashboard/src/services/section.service.ts
+++ src/dashboard/src/services/section.service.ts
@@ -4,6 +4,11 @@ import { SectionDto, type CreateSectionDto } from 'dtos';
 class SectionService {
   private readonly baseUrl = '/api/sections';
 
+  async getAllSections(): Promise<SectionDto[]> {
+    const response = await api.get<SectionDto[]>(this.baseUrl);
+    return response.data;
+  }
+
   async createSection(data: CreateSectionDto): Promise<SectionDto> {
     const response = await api.post<SectionDto>(this.baseUrl, data);
     return response.data;
