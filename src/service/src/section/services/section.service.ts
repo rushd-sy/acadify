e296fdd63f57293c694c1440cfa0d9ecf56fe98c
Adel Obaji <adelobaji23@gmail.com>
2026-09-26 03:32:49 +0300

    feature: implement section update backend

--- src/service/src/section/services/section.service.ts
+++ src/service/src/section/services/section.service.ts
@@ -1,7 +1,11 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 import { SectionRepository } from '../data/section.repository';
 import { SectionMapper } from '../mappers/section.mapper';
-import { CreateSectionDto, SectionDto } from 'dtos';
+import { CreateSectionDto, SectionDto, UpdateSectionDto } from 'dtos';
 
 @Injectable()
 export class SectionService {
@@ -14,6 +18,16 @@ export class SectionService {
     const sections = await this.repository.findAll();
 
     return this.mapper.toDtoList(sections);
+  }
+
+  async findById(id: number): Promise<SectionDto> {
+    const sectionDomain = await this.repository.findById(id);
+
+    if (!sectionDomain) {
+      throw new NotFoundException(`Section with ID ${id} not found`);
+    }
+
+    return this.mapper.toDto(sectionDomain);
   }
 
   async createSection(createSection: CreateSectionDto): Promise<SectionDto> {
@@ -32,5 +46,29 @@ export class SectionService {
     const createdSection = await this.repository.create(sectionDomain);
 
     return this.mapper.toDto(createdSection);
+  }
+
+  async updateSectionById(
+    id: number,
+    data: UpdateSectionDto,
+  ): Promise<SectionDto> {
+    const sectionDomain = await this.repository.findById(id);
+
+    if (!sectionDomain) {
+      throw new NotFoundException(`Section with ID ${id} not found`);
+    }
+
+    if (data.name) {
+      const existingSection = await this.repository.findByName(data.name);
+
+      if (existingSection && existingSection.id !== id) {
+        throw new ConflictException(
+          `Section with name "${data.name}" already exists`,
+        );
+      }
+    }
+    const updatedSection = await this.repository.updateSectionById(id, data);
+
+    return this.mapper.toDto(updatedSection);
   }
 }
